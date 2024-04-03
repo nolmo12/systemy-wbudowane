facb4fc1cf27edb2ef65ff4b8642a17180e3a3f1
Paweł Bąk <92019664+nolmo12@users.noreply.github.com>
2024-04-03 16:03:46 +0200

    Update zad1.c

--- zad1.c
+++ zad1.c
@@ -18,7 +18,7 @@
 #include "xc.h"
 #include "libpic30.h"
 
-void binaryCounter(int *counter)
+void binaryCounter(unsigned int *counter)
 {
     unsigned portValue = 0;
     char currentS6 = 0, currentS7 = 1, currentS4 = 0, prevS6 = 0, prevS7 = 1, prevS4 = 0;
@@ -42,13 +42,144 @@ void binaryCounter(int *counter)
         }
         if(currentS4 - prevS4 == 1)
         {
-            counter++;
+            (*counter)++;
             break;
         }
-        
-        
     }
 }
+
+unsigned int getGrayCode(unsigned int number)
+{
+    return (number ^ (number >> 1));
+}
+
+void grayCounter(unsigned int *counter)
+{
+    unsigned int portValue = 0;
+    unsigned int grayCode = 0;
+    char currentS6 = 0, currentS7 = 1, currentS4 = 0, prevS6 = 0, prevS7 = 1, prevS4 = 0;
+
+    while(1)
+    {
+        grayCode = getGrayCode(portValue);
+        LATA = grayCode;
+        prevS4 = PORTDbits.RD13;
+        prevS6 = PORTDbits.RD6;
+        prevS7 = PORTDbits.RD7;
+        __delay32(150000);
+        currentS4 = PORTDbits.RD13;
+        currentS6 = PORTDbits.RD6;
+        currentS7 = PORTDbits.RD7;
+
+        if(currentS6 - prevS6 == 1)
+        {
+            portValue++;
+        }
+        if(currentS7 - prevS7 == 1)
+        {
+            portValue--;
+        }
+
+        if(currentS4 - prevS4 == 1) 
+        {
+            (*counter)++;
+            break;
+        }
+    }
+}
+
+void decimalToBCD(unsigned int decimal, unsigned int *bcd) 
+{
+    bcd[0] = decimal / 10;
+    bcd[1] = decimal % 10;
+}
+
+void bcdCounter(unsigned int *counter) 
+{
+    unsigned int portValue = 0;
+    unsigned int bcd[2];
+
+    while(1)
+    {
+        decimalToBCD(portValue, bcd);
+        
+
+        LATA = (bcd[0] << 4) | bcd[1];
+        
+        char currentS6 = PORTDbits.RD6;
+        char currentS7 = PORTDbits.RD7;
+        char currentS4 = PORTDbits.RD13;
+
+        __delay32(150000);
+
+        char newS6 = PORTDbits.RD6;
+        char newS7 = PORTDbits.RD7;
+        char newS4 = PORTDbits.RD13;
+
+        if(newS6 - currentS6 == 1) 
+        {
+            portValue++;
+        }
+        if(newS7 - currentS7 == 1) 
+        {
+            portValue--;
+        }
+        if(newS4 - currentS4 == 1) 
+        {
+            (*counter)++;
+            break;
+        }
+    }
+}
+
+void snake(unsigned int *counter)
+{
+    unsigned int portValue = 7;
+    unsigned int bcd[2];
+
+    while(1)
+    {
+        LATA = portValue;
+        
+        char currentS6 = PORTDbits.RD6;
+        char currentS7 = PORTDbits.RD7;
+        char currentS4 = PORTDbits.RD13;
+
+        __delay32(150000);
+
+        char newS6 = PORTDbits.RD6;
+        char newS7 = PORTDbits.RD7;
+        char newS4 = PORTDbits.RD13;
+
+        if(newS6 - currentS6 == 1) 
+        {
+            if(portValue << 1 < 225)
+                portValue = portValue << 1;
+        }
+        if(newS7 - currentS7 == 1) 
+        {
+            if(portValue >> 1 > 6)
+                portValue = portValue >> 1;
+        }
+        if(newS4 - currentS4 == 1) 
+        {
+            (*counter)++;
+            break;
+        }
+    }
+}
+
+void queue(unsigned int *counter)
+{
+    
+}
+
+
+void randomNumberGenerator(unsigned int *counter)
+{
+    
+}
+
 
 int main(void)
 {
@@ -61,7 +192,7 @@ int main(void)
     
     while(1)
     {
-        LATA = counter;
+        LATA = 0;
         prevS4 = PORTDbits.RD13;
         __delay32(150000);
         currentS4 = PORTDbits.RD13;
@@ -71,20 +202,29 @@ int main(void)
             counter++;
         }
         
-        switch(counter % 6)
+        switch(counter % 9)
         {
             case 0:
                 binaryCounter(&counter);
                 break;
             case 1:
+                grayCounter(&counter);
                 break;
             case 2:
+                bcdCounter(&counter);
                 break;
             case 3:
+                snake(&counter);
                 break;
             case 4:
                 break;
             case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
                 break;
             default:
                 break;
